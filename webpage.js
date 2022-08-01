72daab17609d559813b9596cbb2c84999caa8024
SYY-YYS <108676973+SYY-YYS@users.noreply.github.com>
2022-08-02 00:08:57 +0800

    first draft

---
+++ webpage.js
@@ -0,0 +1,15 @@
+
+document.title = "Samuel Siu's Webpaage";
+
+// function clickIcon(event) {
+//     console.log(event.target);
+//     let nav = event.target;
+//     nav.style.left = '0px';
+// }
+
+function hidemenu() {
+    document.getElementById('navlink').style.left = '-180px';
+}
+function showmenu() {
+    document.getElementById('navlink').style.left = '0px';
+}
